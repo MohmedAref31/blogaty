d33d6b4489c7cf7bd631814a776cbce156f446fa
MohmedAref31 <mohmedwork2003@gmail.com>
2023-09-24 17:07:00 +0300

    add db to env
    add db to env

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 
 const dbConnection = async () =>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/projectTask")
+    await mongoose.connect(process.env.DB)
             .then(()=>console.log("db connection good"))
             .catch(()=>console.log("faild to connect to db"))
 }
